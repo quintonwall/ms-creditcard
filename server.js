6e356c3826aa8802133d7d1548737b20b634e061
Quinton Wall <qwall@salesforce.com>
2016-06-08 10:59:06 -0700

    working app

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express'),
+bodyParser = require('body-parser'),
+compression = require('compression'),
+creditcard = require('./server/creditcard'),
+app = express();
+
+app.set('port', process.env.PORT || 5000);
+
+app.use(bodyParser.json());
+app.use(compression());
+
+app.use('/', express.static(__dirname + '/www'));
+
+app.get('/validatecardnumber/:id', creditcard.validatenumber);
+app.post('/validatecreditcard/', creditcard.validatecard);
+
+app.listen(app.get('port'), function() {
+  console.log('Credit Card server listening on port '+app.get('port'));
+});
---
+++ server/creditcard.js
@@ -0,0 +1,61 @@
+
+/*
+
+test method that simply returns true for any number.
+
+ host:port/validatecardnumber/id=1234
+*/
+function validatenumber(req,res,next) {
+  var ccnum = req.params.id,
+  promise;
+
+  var CreditCard = require('credit-card');
+
+  var card = {
+    cardType: 'VISA',
+    number: '4111111111111111',
+    expiryMonth: '03',
+    expiryYear: '2100',
+    cvv: '123'
+  };
+  var validation = CreditCard.validate(card);
+
+  console.log('Credit card number is '+ccnum);
+
+
+  return res.send(JSON.stringify("true"));
+}
+
+
+
+
+/*
+  Accept a JSON payload. see testcc.json for example
+
+  Uses https://www.npmjs.com/package/credit-card
+
+  For a list of test credit cards, see:
+   http://www.paypalobjects.com/en_US/vhelp/paypalmanager_help/credit_card_numbers.htm
+*/
+function validatecard(req,res,next) {
+
+    var data = req.body;
+
+
+  var CreditCard = require('credit-card');
+
+  var card = {
+    cardType: data.cardType,
+    number: data.number,
+    expiryMonth: data.expiryMonth,
+    expiryYear: data.expiryYear,
+    cvv: data.ccv
+  };
+  var validation = CreditCard.validate(card);
+
+
+  return res.send(JSON.stringify(validation));
+}
+
+exports.validatenumber = validatenumber;
+exports.validatecard = validatecard;
